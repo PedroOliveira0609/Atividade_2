bc6a4369f7a2eff96bcd4fca6f4683a13331f7d5
PedroOliveira0609 <90793896+PedroOliveira0609@users.noreply.github.com>
2022-05-29 18:18:06 -0300

    Atividade_2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double cotacao = 5.13;
+
+Console.WriteLine($"A cotação do euro hoje é: R$ {cotacao}");
+Console.WriteLine($"Quantos euros você irá comprar?");
+int compraeuro = Convert.ToInt32(Console.ReadLine());
+
+double vcompra = cotacao * compraeuro;
+
+Console.WriteLine($"O valor da compra de {compraeuro} euros em reais são: R$ {vcompra}");
